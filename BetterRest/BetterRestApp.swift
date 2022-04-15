a193ecc739530688a96dac2bb96661470869526c
Alivia Fairchild <aliviafairchild@Alivias-Air.hsd1.ut.comcast.net>
2022-04-15 10:02:45 -0600

    are you there

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alivia Fairchild on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
